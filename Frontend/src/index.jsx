680d8f473a6ac707b8328e3953136b0a9562c783
Minojini <minojini05@gmail.com>
2025-02-19 15:30:50 +0530

    added

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Sidemenu from './Sidemenu'
-import Login from './Login'
+import Signin from './Signin'
 import { Tenant } from './Tenant/Tenant'
 import Lease from './Lease/Lease'
 import Property from './Property/Property'
@@ -21,7 +21,7 @@ const App=()=>{
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />, 
+    element: <Signin />, 
   },
   {
     path: "/user",
